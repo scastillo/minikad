5a0118295f56b0120bc48d9f26452c7a69aed6ad
abuiles <abuiles@parasito.(none)>
2010-10-15 13:41:06 -0500

    minor changes in superpeed

--- MiniKadSuperPeer.cc
+++ MiniKadSuperPeer.cc
@@ -5,48 +5,52 @@
 
 using namespace std;
 
-// Define_Module(MiniKadSuperPeer);
+Define_Module(MiniKadSuperPeer);
 
-// void MiniKadSuperPeer::initializeOverlay(int stage)
-// {
-//     return;
-// }
-// void PubSubLobby::finishOverlay()
-// {
-//   return;
-// }
+void MiniKadSuperPeer::initializeOverlay(int stage)
+{
+      // because of IPAddressResolver, we need to wait until interfaces are registered,
+    // address auto-assignment takes place etc.
+    if(stage != MIN_STAGE_OVERLAY) return;
 
-// void MiniKadSuperPeer::handleTimerEvent(cMessage* msg)
-// {
-//   return;
-// }
+    thisNode.setKey( OverlayKey::random() );
+    WATCH( "Initializing SuperPeer" );
+}
 
-// void MiniKadSuperPeer::handleUDPMessage(BaseOverlayMessage* msg)
-// {
-//   return;
-// }
+void MiniKadSuperPeer::handleTimerEvent(cMessage* msg)
+{
+  WATCH( "Timer" );
+}
 
-// bool MiniKadSuperPeer::handleRpcCall(BaseCallMessage* msg)
-// {
-//   return;
-// }
+void MiniKadSuperPeer::handleUDPMessage(BaseOverlayMessage* msg)
+{
+  WATCH( "UPDMessage" );
+}
+bool MiniKadSuperPeer::handleRpcCall(BaseCallMessage* msg)
+{
+  WATCH( "RPCCall" );
+  return true;
+}
 
-// void MiniKadSuperPeer::handleRpcResponse(BaseResponseMessage *msg,
-//                                     cPolymorphic* context,
-//                                     int rpcId, simtime_t rtt)
-// {
-//   return;
-// }
+void MiniKadSuperPeer::handleRpcResponse(BaseResponseMessage *msg,
+                                    cPolymorphic* context,
+                                    int rpcId, simtime_t rtt)
+{
+  WATCH( "RPCResponse" );
+}
 
-// void MiniKadSuperPeer::handleRpcTimeout (BaseCallMessage *msg,
-//                                     const TransportAddress &dest,
-//                                     cPolymorphic* context, int rpcId,
-//                                     const OverlayKey &destKey)
-// {
-//   return;
-// }
+void MiniKadSuperPeer::handleRpcTimeout (BaseCallMessage *msg,
+                                    const TransportAddress &dest,
+                                    cPolymorphic* context, int rpcId,
+                                    const OverlayKey &destKey)
+{
+  WATCH( "RPCTimeOut" );
+}
 
-
-// MiniKadSuperPeer::~MiniKadSuperPeer()
-// {
-// }
+void MiniKadSuperPeer::finishOverlay()
+{
+   WATCH( "FinishOverlay" );
+}
+MiniKadSuperPeer::~MiniKadSuperPeer()
+{
+}
--- MiniKadSuperPeer.h
+++ MiniKadSuperPeer.h
@@ -7,24 +7,23 @@
 
 class MiniKadSuperPeer : public BaseOverlay
 {
-    /* public: */
-    /*     // OMNeT++ */
-    /*     virtual ~MiniKadSuperPeer(); */
-    /*     virtual void initializeOverlay(int stage); */
-    /*     virtual void finishOverlay(); */
-    /*     virtual void handleUDPMessage(BaseOverlayMessage* msg); */
-    /*     virtual void handleTimerEvent(cMessage* msg); */
+      public:
+        // OMNeT++
+        virtual ~MiniKadSuperPeer();
+        virtual void initializeOverlay(int stage);
+        virtual void finishOverlay();
+        virtual void handleUDPMessage(BaseOverlayMessage* msg);
+        virtual void handleTimerEvent(cMessage* msg);
 
-    /*     virtual bool handleRpcCall(BaseCallMessage* msg); */
-    /*     virtual void handleRpcResponse(BaseResponseMessage *msg, */
-    /*                                    cPolymorphic* context, */
-    /*                                    int rpcId, simtime_t rtt); */
-    /*     virtual void handleRpcTimeout(BaseCallMessage *msg, */
-    /*                                   const TransportAddress &dest, */
-    /*                                   cPolymorphic* context, int rpcId, */
-    /*                                   const OverlayKey &destKey); */
+        virtual bool handleRpcCall(BaseCallMessage* msg);
+        virtual void handleRpcResponse(BaseResponseMessage *msg,
+                                       cPolymorphic* context,
+                                       int rpcId, simtime_t rtt);
+        virtual void handleRpcTimeout(BaseCallMessage *msg,
+                                      const TransportAddress &dest,
+                                      cPolymorphic* context, int rpcId,
+                                      const OverlayKey &destKey);
 
-    protected:
+
 };
-
 #endif
